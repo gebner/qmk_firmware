96bde793fc61190e44a7c760c73c61d52cff1472
Gabriel Ebner <gebner@gebner.org>
2019-03-26 12:29:38 +0100

    kbd75: add ;-activated movement layer

--- keyboards/kbdfans/kbd75/keymaps/gebner/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/gebner/keymap.c
@@ -1,22 +1,24 @@
 #include QMK_KEYBOARD_H
 
+#define KC_SCL_M LT(1, KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ansi_1u(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_INS,   KC_DEL,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCL_M, KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,    KC_END,
-    MO(1),    KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(2),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    MO(1),    KC_LGUI,  KC_LALT,                                MT(MOD_LSFT, KC_SPC),                   KC_RALT,  MO(2),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT_ansi_1u(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,                      _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,
+    _______,  _______,  KC_WH_L,  KC_WH_U,  KC_WH_D,  KC_WH_R,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,  _______,            _______,
+    _______,  _______,  KC_MS_L,  KC_MS_U,  KC_MS_D,  KC_MS_R,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,                      _______,  _______,
+    _______,  _______,  _______,  KC_BTN2,  KC_BTN1,  KC_BTN3,  _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,
     _______,  _______,  _______,                                _______,                                _______,  MO(3),    _______,  _______,  _______,  _______
   ),
 
