329c82f942173b2215863d1378b8fd7967cf8327
Gabriel Ebner <gebner@gebner.org>
2019-05-21 21:36:33 +0200

    alpengurke: move ,./ below kl;

--- keyboards/alpengurke/keymaps/default/keymap.c
+++ keyboards/alpengurke/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUM] = LAYOUT_ortho_3x10(
     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
     KC_GRV, KC_NO, KC_MINS, KC_EQL, KC_BSLS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_DEL,
-    _______, _______, _______, _______, _______, _______, KC_COMM, KC_DOT, KC_SLSH, KC_ESC
+    _______, _______, _______, _______, _______, _______, _______, KC_COMM, KC_DOT, KC_SLSH
   ),
 
   [_FKEYS] = LAYOUT_ortho_3x10(
