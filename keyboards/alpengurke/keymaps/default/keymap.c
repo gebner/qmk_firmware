44cf6a2b27bff3baa701698474918ee6ac6d3d1d
Gabriel Ebner <gebner@gebner.org>
2019-05-21 21:05:32 +0200

    alpengurke: add esc on the left

--- keyboards/alpengurke/keymaps/default/keymap.c
+++ keyboards/alpengurke/keymaps/default/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOVM] = LAYOUT_ortho_3x10(
 		KC_TAB,  KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,    KC_INS,
-		xxxxxxx, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  _______,
+		KC_ESC,  KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  _______,
 		_______, _______, KC_BTN2, KC_BTN1, KC_BTN3,  _______, KC_VOLD, KC_VOLU, KC_MPLY,   xxxxxxx
   ),
 
