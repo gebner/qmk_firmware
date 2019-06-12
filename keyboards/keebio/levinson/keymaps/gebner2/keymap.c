99f5e2601401ed2cfa7d3260dbd92de800498fbe
Gabriel Ebner <gebner@gebner.org>
2019-06-12 18:56:46 +0200

    Update alternative levinson layout.

--- keyboards/keebio/levinson/keymaps/gebner2/keymap.c
+++ keyboards/keebio/levinson/keymaps/gebner2/keymap.c
@@ -14,6 +14,7 @@ enum {
 
 #define KC_X_LALT MT(MOD_LALT, KC_X)
 #define KC_C_LCTL MT(MOD_LCTL, KC_C)
+#define KC_A_LCTL MT(MOD_LCTL, KC_A)
 #define KC_B_LGUI MT(MOD_LGUI, KC_B)
 #define KC_N_RALT MT(MOD_RALT, KC_N)
 #define KC_M_RCTL MT(MOD_RCTL, KC_M)
@@ -26,18 +27,18 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_kc(
-1, 2, 3, 4, 5, MINS, EQL, 6, 7, 8, 9, 0,
-Q, W, E, R, T, LBRC, RBRC, Y, U, I, O, P,
-A, S, D, F, G, QUOT, BSLS, H, J, K, L, SCLN_MOVM,
-Z, X_LALT, C_LCTL, V, B_LGUI, COMM_MOVM,
-  DOT, SPC_LSFT, N_RALT, M_RCTL, BSPC, ENT
+MINS, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, EQL,
+LBRC, Q, W, E, R, T, Y, U, I, O, P, RBRC,
+QUOT, A_LCTL, S, D, F, G, H, J, K, L, SCLN_MOVM, BSLS,
+COMM_MOVM, Z, X_LALT, C_LCTL, V, B_LGUI,
+  SPC_LSFT, N_RALT, M_RCTL, BSPC, ENT, DOT
 ),
 
 [_MOVM] = LAYOUT_kc(
-F1, F2, F3, F4, F5, F11, F12, F6, F7, F8, F9, F10,
-TAB, WH_L, WH_U, WH_D, WH_R, PSCR, INS, HOME, PGDN, PGUP, END, ,
-ESC, MS_L, MS_U, MS_D, MS_R, , , LEFT, DOWN, UP, RIGHT, ,
-, , BTN2, BTN1, BTN3, TILD, SLSH, , VOLD, VOLU, MPLY,
+F11, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F12,
+PSCR, TAB, WH_L, WH_U, WH_D, WH_R, HOME, PGDN, PGUP, END, , INS,
+, ESC, MS_L, MS_U, MS_D, MS_R, LEFT, DOWN, UP, RIGHT, , ,
+TILD, , , BTN2, BTN1, BTN3, , VOLD, VOLU, MPLY, , SLSH
 ),
 
 };
