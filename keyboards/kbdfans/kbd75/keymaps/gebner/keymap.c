eac8b38e083b68919c82f403d096f27da22869d1
Gabriel Ebner <gebner@gebner.org>
2019-06-12 18:57:54 +0200

    kbd75: remove mouse only layer

--- keyboards/kbdfans/kbd75/keymaps/gebner/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/gebner/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     MT(MOD_LCTL, KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCL_M, KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(2),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT_ansi_1u(
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  KC_WH_L,  KC_WH_U,  KC_WH_D,  KC_WH_R,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,  _______,            _______,
     _______,  _______,  KC_MS_L,  KC_MS_U,  KC_MS_D,  KC_MS_R,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,                      _______,  _______,
     _______,  _______,  _______,  KC_BTN2,  KC_BTN1,  KC_BTN3,  _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,
-    _______,  _______,  _______,                                _______,                                _______,  MO(3),    _______,  _______,  _______,  _______
+    MO(3),  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______
   ),
 
   [2] = LAYOUT_ansi_1u(
